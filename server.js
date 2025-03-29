fd23c555b49d3fc80cca7d5bf9b8f8d7a6e4cb45
caofayou <2017954256@qq.com>
2025-03-30 07:31:27 +0800

    将eslint添加到项目

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get('/products', async (req, res) => {
 });
 
 // Error handling middleware
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
